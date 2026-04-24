ece163f4b9a38d9b90b52945f7cfbe2378bed7de
Habibur Rahman <habiburkuetacbd@gmail.com>
2026-04-24 20:11:34 +0600

    Add about route

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/home', (req, res) => {
     res.send('Welcome to the Home Page!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('This is the About Page!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
